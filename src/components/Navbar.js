8510aa1d782b7fad8ac0c8708779d058399c419c
Fırtına Küçük <firtinapnp@fs0c13ty-Mac.local>
2018-10-03 00:48:23 +0300

    Navbar Done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,27 @@ import M from 'materialize-css/dist/js/materialize.min.js';
 class Navbar extends Component {
     constructor(props){
         super(props);
-        var instances = M.Dropdown.init();
+        this.state={ dropInstance:null};
+    }
+    componentDidMount(){
+        M.Dropdown.init(this.dropdown, {
+            alignment:"right",
+            constrainWidth:false,
+            coverTrigger:false
+        });
+        var instance=M.Dropdown.getInstance(this.dropdown);
+        this.setState(()=>({
+            dropInstance: instance
+        }));
+        window.addEventListener("resize", this.handleResize.bind(this));
+    }
+    componentWillUnmount(){
+        window.removeEventListener("resize", this.handleResize.bind(this));
+    }
+    handleResize(){
+        if(this.state.dropInstance.isOpen){
+            this.state.dropInstance.close();
+        }
     }
     render() {
         return (
@@ -14,10 +34,10 @@ class Navbar extends Component {
                 <div className="navbar-fixed">
                     <nav>
                         <div className="nav-wrapper blue-grey darken-3">
-                            <a href="#!" className="brand-logo left" style={{marginLeft:"1em"}}><span><img src={logo} alt="Math204" style={{height:"4rem", marginTop:"0.5rem"}}/></span></a>
-                            <a href="#" data-activates="mobile-demo" className="button-collapse right"><i className="material-icons">menu</i></a>
+                            <div className="brand-logo left" style={{marginLeft:"1em"}}><span><img src={logo} alt="Math204" style={{height:"4rem", marginTop:"0.5rem"}}/></span></div>
+                            <a href="#" data-target="mobile-demo" ref={ (dropdown) => {this.dropdown = dropdown} } className="hide-on-large-only dropdown-trigger right"><i className="material-icons">menu</i></a>
                             <ul id="navbar-pc" className="right hide-on-med-and-down" style={{marginRight:"1.2em"}}>
-                                <li id="home-pc"><NavLink exact activeClassName="active light-blue darken-1" to="/~math204/">Home</NavLink></li>
+                                <li><NavLink exact activeClassName="active light-blue darken-1" to="/~math204/">Home</NavLink></li>
                                 <li><NavLink activeClassName="active light-blue darken-1" to="/~math204/announce">Announcements</NavLink></li>
                                 <li><NavLink activeClassName="active light-blue darken-1" to="/~math204/syllabus">Syllabus</NavLink></li>
                                 <li><NavLink activeClassName="active light-blue darken-1" to="/~math204/info">Info &amp; Office Hours</NavLink></li>
@@ -26,8 +46,8 @@ class Navbar extends Component {
                         </div>
                     </nav>
                 </div>
-                <ul className="dropdown-content blue-grey darken-3" id="mobile-demo">
-                    <li id="home-mob" ><NavLink exact activeClassName="active light-blue darken-1" to="/~math204/" className="white-text">Home</NavLink></li>
+                <ul className="dropdown-content hide-on-large-only blue-grey darken-3" id="mobile-demo">
+                    <li><NavLink exact activeClassName="active light-blue darken-1" to="/~math204/" className="white-text">Home</NavLink></li>
                     <li><NavLink activeClassName="active light-blue darken-1" to="/~math204/announce" className="white-text">Announcements</NavLink></li>
                     <li><NavLink activeClassName="active light-blue darken-1" to="/~math204/syllabus" className="white-text">Syllabus</NavLink></li>
                     <li><NavLink activeClassName="active light-blue darken-1" to="/~math204/info" className="white-text">Info &amp; Office Hours</NavLink></li>
